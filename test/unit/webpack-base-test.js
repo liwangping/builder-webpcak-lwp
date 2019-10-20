c2d6c64c76953e687f97cb443ba4f0302435b0ba
liwangping <liwangping@lightinthebox.com>
2019-10-20 17:13:35 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('webpack.base.js test case', () => {
 
     console.log(baseConfig);//编写用例
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, '/Users/lt/Desktop/webpack/Build_webpack/test/smoke/template/src/index/index.js');
-        assert.equal(baseConfig.entry.search, '/Users/lt/Desktop/webpack/Build_webpack/test/smoke/template/src/search/index.js');
+        assert.equal(baseConfig.entry.index.indexof('builder-webpcak-lwp/test/smoke/template/src/index/index.js') > -1, true );
+        assert.equal(baseConfig.entry.searchindexof('builder-webpcak-lwp/test/smoke/template/src/search/index.js') > -1, true);
     })
 })
